519e956bfc4a5343df2da8153212057013fb15f4
Sıla Gülaştı <gulastisila@gmail.com>
2026-01-12 21:48:16 +0300

    Clean up project notes and define sensor pins
    Removed project notes and added ultrasonic sensor pin definitions.

--- ultrasonic_led_warning.ino
+++ ultrasonic_led_warning.ino
@@ -1,32 +1,4 @@
-[21:29, 12.01.2026] SılAşkım♥️: ## Projects
-
-### Student Management System (C)
-A console-based application that allows:
-- Adding students
-- Listing students
-- Calculating class average
-- Finding the top student
-
-Concepts used:
-- Structs
-- Arrays
-- Functions
-- Menu-driven programming
-[21:32, 12.01.2026] SılAşkım♥️: README’ye bu projeyi ekleyeceğiz
-Arduino’da sensörlü projeye geçeceğiz
-CV’ye “Projects” bölümünü yazacağız
-[21:32, 12.01.2026] SılAşkım♥️: 1️⃣ Dosya okuma/yazma (FILE)* ekleyip projeyi LEVEL UP
-2️⃣ Arduino’da sensörlü proje (ultrasonik / LDR)
-3️⃣ CV’de Projects bölümünü parlatma
-[21:34, 12.01.2026] SılAşkım♥️: Arduino’da sensörlü (ultrasonik / LDR) proje
-GitHub profilinde harbi dolu görüntü
-CV’ye Projects bölümü yazma
-Bunu ekleyince bana yaz:
-“README’ye Projects eklendi”
-[21:34, 12.01.2026] SılAşkım♥️: Arduino’da sensörlü proje (ultrasonik / LDR)
-GitHub profilin artık harbi dolu
-CV’ye Projects bölümü ekleme
-[21:36, 12.01.2026] SılAşkım♥️: #define trigPin 9
+#define trigPin 9
 #define echoPin 10
 #define ledPin 13
 
